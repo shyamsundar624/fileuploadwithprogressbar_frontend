e087109db7661314d7542c360203551388247509
shyamsundar624 <shyamsundaryadav624@gmail.com>
2024-11-13 06:32:42 +0530

    complete frontend

---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+private baseUlr="http://localhost:9096";
+
+  constructor(private http:HttpClient) { }
+
+  upload(file:File):Observable<HttpEvent<any>>{
+    const formData:FormData=new FormData();
+
+    formData.append('file',file);
+    const req=new HttpRequest('POST',`${this.baseUlr}/upload`,formData,{
+      reportProgress:true,
+      responseType: 'json'
+    });
+    return this.http.request(req);
+  }
+
+  getFiles():Observable<any>{
+    return this.http.get(`${this.baseUlr}/files`);
+  }
+}
